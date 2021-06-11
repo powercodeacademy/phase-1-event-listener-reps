473a88912ef493b2a9e927f4c904800ccfb6a58c
smkopp92 <smkopp92@gmail.com>
2021-06-11 14:35:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Overview
+
+These exercises are to practice DOM manipulation. Core concepts to practice are using query selectors, updating elements on the page, and iterating.
+
+To get started, open up the `index.html` page in a web browser, and code your solutions in the `index.js` file.
+
---
+++ src/index.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+/*
+  These exercises are to practice DOM manipulation. Core concepts to practice
+  are using query selectors, updating elements on the page, and iterating.
+
+  To get started, open up the `index.html` page in a web browser,
+  and code your solutions in the `index.js` file.
+*/
+
+/* -------------- [FIRST SECTION - PHOTO] --------------- */
+
+const comments = [
+  "Beautiful shot!",
+  "Great colors!",
+  "Wooooooooooooow!",
+  "This is amazing!"
+]
+
+// 1. Find the url for an image of your choice, select the dog image, and replace it with yours!
+
+// 2. Select the text "A Good Dog", and replace with your own title
+
+// 3. Select the "0 Likes", and replace it with however many likes your photo deserves!
+
+// 4. Select the text "The goodest dog!", and replace it with your own
+
+// 5. Iterate through the `comments` array above, create a <li> for each comment, and
+//    append it to the <ul> of existing comments
+
+/* -------------- [SECOND SECTION - BOOK] --------------- */
+
+const reviews = [
+  "This book is life-changing wow!",
+  "WOW so funny",
+  "I read this book in a book club and learned some things"
+]
+
+// 6. Select and replace "Scrum" with a book of your choosing
+
+// 7. Find a photo of the book cover, and select/replace the Scrum cover image src with yours.
+
+// 8. Select and replace the Scrum author(s) with your own (hint: this is the <h3> tag)
+
+// 9. Select and replace the description with your own
+
+// 10. Append the list of reviews (above) to the `bookInfo` div.
+
+/* -------------- [THIRD SECTION - MOVIE] --------------- */
+
+// 11. Select and replace the title "Inception" with a movie of your choice.
+
+// 12. Find a photo of the movie, and select/replace the Inception image src with yours.
+
+// 13. Select and replace the Runtime with the runtime of your movie.
+
+// 14. Select and replace the text "Surely this movie has a director" with your movie's director.
+
+// 15. Select and replace the movie's description (in the textarea tag) with your own movie.
+
+})
+
