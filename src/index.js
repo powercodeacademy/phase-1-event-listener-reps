fc5714cc1165ddf3f4fa9f9ab88b5d67ea4b2090
smkopp92 <smkopp92@gmail.com>
2021-06-14 09:41:36 -0400

    Prepare event listener practice

--- src/index.js
+++ src/index.js
@@ -7,55 +7,51 @@ document.addEventListener("DOMContentLoaded", () => {
   and code your solutions in the `index.js` file.
 */
 
-/* -------------- [FIRST SECTION - PHOTO] --------------- */
+  // -------------- [FIRST SECTION - PHOTO] ---------------
 
-const comments = [
-  "Beautiful shot!",
-  "Great colors!",
-  "Wooooooooooooow!",
-  "This is amazing!"
-]
+  const increaseLikes = () => {
+    const likesElement = document.querySelector("#instaLikes") // Finds the like count
+    const newLikes = parseInt(likesElement.innerHTML) + 1 // Parses the number of likes out of the page text and adds 1
+    likesElement.innerHTML = `${newLikes} likes` // // Adds the new number of likes back onto the page
+  }
 
-// 1. Find the url for an image of your choice, select the dog image, and replace it with yours!
+  const toggleTitle = () => {
+    const titleElement = document.querySelector("#instaTitle") // Finds the title of the instagram post
+    titleElement.classList.toggle("hidden") // Toggles a hidden class which hides/shows the text (check out the class in the css file!)
+  }
 
-// 2. Select the text "A Good Dog", and replace with your own title
+  const alertClick = () => alert("You clicked a comment!") // Alerts with static text
 
-// 3. Select the "0 Likes", and replace it with however many likes your photo deserves!
+  const changeToRed = event => event.target.classList.add("red") // Changes the color of what was clicked on to red (check out the class in the css file!)
 
-// 4. Select the text "The goodest dog!", and replace it with your own
+  const alertFormSubmission = (event) => {
+    event.preventDefault() // Prevents the form from doing the default html submit that triggers a page reload
+    alert(`You submitted: ${event.target.description.value}`) // Alerts with dynamic text depending on what is in the text box
+  }
 
-// 5. Iterate through the `comments` array above, create a <li> for each comment, and
-//    append it to the <ul> of existing comments
+  // 1. Add an event listener to the heart that calls the "increaseLikes" function when clicked
+
+  // 2. Add an event listener to the dog image that calls the "toggleTitle" function when clicked
+
+  // 3. Add an event listener to each comment in the comment section that calls the "alertClick" function when clicked
+
+  // 4. Add an event listener to each comment in the comment section that calls an anonymous function which calls "changeToRed(event)" when clicked.
+  // (HINT: You will need to pass in the event in the anonymous arrow function)
+  // (i.e someElement.addEventListener("someAction", (event) => someFunction(event)))
 
 /* -------------- [SECOND SECTION - BOOK] --------------- */
 
-const reviews = [
-  "This book is life-changing wow!",
-  "WOW so funny",
-  "I read this book in a book club and learned some things"
-]
+  // 5. Add an event listener to the book image that calls the "alertClick" function when clicked.
 
-// 6. Select and replace "Scrum" with a book of your choosing
+  // 6. Add an event listener to the title of the book that calls an anonymous function which calls "changeToRed(event)" when clicked.
+  // (HINT: You will need to pass in the event in the anonymous arrow function)
 
-// 7. Find a photo of the book cover, and select/replace the Scrum cover image src with yours.
-
-// 8. Select and replace the Scrum author(s) with your own (hint: this is the <h3> tag)
-
-// 9. Select and replace the description with your own
-
-// 10. Append the list of reviews (above) to the `bookInfo` div.
 
 /* -------------- [THIRD SECTION - MOVIE] --------------- */
 
-// 11. Select and replace the title "Inception" with a movie of your choice.
+  // 7. Add an event listener to the movie image that calls the "alertClick" function when clicked.
 
-// 12. Find a photo of the movie, and select/replace the Inception image src with yours.
-
-// 13. Select and replace the Runtime with the runtime of your movie.
-
-// 14. Select and replace the text "Surely this movie has a director" with your movie's director.
-
-// 15. Select and replace the movie's description (in the textarea tag) with your own movie.
-
+  // 8. Add an event listener to the form that calls an anonymous function that calls the "alertFormSubmission" function when submitted.
+  // (HINT: You will need to pass in the event in the anonymous arrow function)
 })
 
